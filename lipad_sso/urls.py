7af46432700e54c158d2e287ef9a567da542be62
Carlo Santos <casantos@dream.upd.edu.ph>
2016-06-22 14:30:52 +0800

    added mama_cas to installed apps; updated urls.py

--- lipad_sso/urls.py
+++ lipad_sso/urls.py
@@ -1,10 +1,18 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
+#urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'lipad_sso.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+#    url(r'^admin/', include(admin.site.urls)),
+#)
+
+urlpatterns = [
+	url(r'^admin/', admin.site.urls),
+    url(r'^', include('mama_cas.urls')),
+    url(r'^cas/',include('mama_cas.urls')),
+    url(r'^$', RedirectView.as_view(url='/login')),
+
+]
